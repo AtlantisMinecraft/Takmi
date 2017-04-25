9b728501438913320d46f235f8e54d8de7959376
ReyADayer <reyaddayer@gmail.com>
2017-04-25 18:33:46 +0900

    🌱 first commit

---
+++ src/main/java/net/atlantis/takmisample/TakmiSample.java
@@ -0,0 +1,17 @@
+package net.atlantis.takmisample;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TakmiSample extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
